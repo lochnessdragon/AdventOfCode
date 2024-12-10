25154e460b02f9163131bb2b822b84c45bed1886
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2024-12-09 23:44:03 -0600

    2024 - Day 10, I had some thoughts

--- 2024/day10/main.swift
+++ 2024/day10/main.swift
@@ -25,51 +25,44 @@ extension Collection {
 }
 
 // recursive flood fill at each 0?
-func countTrails(startAt location: Position, on topographicMap: [[Int]], found: inout Set<Position>) -> Int {
-	if topographicMap[safe: location.y]?[safe: location.x] == 9 && !found.contains(Position(x: location.x, y: location.y)) {
-		found.insert(Position(x: location.x, y: location.y))
-		return 1
-	} else {
-		var subcount = 0
-
-		for direction in [Position(x: -1, y: 0), Position(x: 1, y: 0), Position(x: 0, y: -1), Position(x: 0, y: 1)] {
-			let nextLocation = location + direction
-			if (topographicMap[safe: nextLocation.y]?[safe: nextLocation.x] ?? 0) - 1 == topographicMap[safe: location.y]?[safe: location.x] {
-				subcount += countTrails(startAt: nextLocation, on: topographicMap, found: &found)
-			}
-		}
-
-		return subcount
-	}
-}
-
-func countRatings(startAt location: Position, on topographicMap: [[Int]]) -> Int {
+func countTrailsAndRatings(startAt location: Position, on topographicMap: [[Int]], found: inout Set<Position>) -> (Int, Int) {
 	if topographicMap[safe: location.y]?[safe: location.x] == 9 {
-		return 1
+		var trailsAndRatings = (0, 1)
+		if !found.contains(Position(x: location.x, y: location.y)) {
+			// trails increase
+			found.insert(Position(x: location.x, y: location.y))
+			trailsAndRatings.0 += 1
+		}
+
+		return trailsAndRatings
 	} else {
-		var subcount = 0
+		var subTrailsAndRatings = (0, 0)
 
 		for direction in [Position(x: -1, y: 0), Position(x: 1, y: 0), Position(x: 0, y: -1), Position(x: 0, y: 1)] {
 			let nextLocation = location + direction
 			if (topographicMap[safe: nextLocation.y]?[safe: nextLocation.x] ?? 0) - 1 == topographicMap[safe: location.y]?[safe: location.x] {
-				subcount += countRatings(startAt: nextLocation, on: topographicMap)
+				let (trails, ratings) = countTrailsAndRatings(startAt: nextLocation, on: topographicMap, found: &found)
+				subTrailsAndRatings.0 += trails
+				subTrailsAndRatings.1 += ratings
 			}
 		}
 
-		return subcount
+		return subTrailsAndRatings
 	}
 }
 
 var part_1 = 0
 var part_2 = 0
+
 for row in 0..<topographicMap.count {
 	for column in 0..<topographicMap[row].count {
 		if topographicMap[row][column] == 0 {
 			// trailhead!
 			// heya swifties, do ya like this naming convention?
 			var found: Set<Position> = []
-			part_1 += countTrails(startAt: Position(x: column, y: row), on: topographicMap, found: &found)
-			part_2 += countRatings(startAt: Position(x: column, y: row), on: topographicMap)
+			let (trails, ratings) = countTrailsAndRatings(startAt: Position(x: column, y: row), on: topographicMap, found: &found)
+			part_1 += trails
+			part_2 += ratings
 		}
 	}
 }
